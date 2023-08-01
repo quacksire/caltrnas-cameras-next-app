2b602397373fc0cdf7f39365d3112c3e657cf009
Quacksire <sam@quacksire.dev>
2023-08-01 02:16:24 -0700

    LMAOOOO fix v2

--- app/cameras/layout.tsx
+++ app/cameras/layout.tsx
@@ -2,10 +2,11 @@ import MaxWidthWrapper from "@/components/max-width-wrapper";
 import TopContent from "@/components/TopContent";
 
 
+// @ts-ignore
 export default function Layout({children}) {
     return (
         <MaxWidthWrapper className="text-center align-middle h-full">
-            <TopContent link={'cameras'} />
+            <TopContent/>
             <div className={'m-3'}>
                 {children}
             </div>
